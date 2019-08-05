63a648eb4192f53de4d6cbd7602c87ac3eded16b
Krom <rramin125@gmail.com>
2019-08-05 17:50:06 -0400

    added http requests

--- lib/ninja.ex
+++ lib/ninja.ex
@@ -5,7 +5,8 @@ defmodule OgSniper.Ninja do
               mojang_password: nil,
               minecraft_email: nil,
               minecraft_password: nil,
-              giftcode: nil
+              giftcode: nil,
+              captcha_token: nil
 
     use GenServer
 
@@ -22,22 +23,30 @@ defmodule OgSniper.Ninja do
         {:ok, struct(__MODULE__, state)}
     end
 
-    def handle_info({:start_sniping_process}, state) do
+    def handle_info({:start_sniping_process}, state) do  # http requests and all that, collect the cookie too
         # this is where we need to start signing into mojang 
-        snipe_in = state.snipe_at_timestamp - :os.system_time(:second)
         IO.puts ("It's time to log into Mojang/Minecraft for the account #{state.name_to_snipe}")
 
-        # http requests and all that, collect the cookie too
+        snipe_in = state.snipe_at_timestamp - :os.system_time(:second)
+        
+        # Process.send(self(), {:set_captcha_token})
 
-        %HTTPoison.Response{body: cookie} = HTTPoison.get("https://my.minecraft.net/en-us/login/?return_url=/en-us/redeem/minecraft/")
+        OgSniper.Utils.send_captcha
 
-        cookie
-        |> Poison.decode!
 
         Process.send_after(self(), {:snipe1}, round(snipe_in * 1000))
 
         {:noreply, state}
     end
+
+    # def handle_cast({:set_captcha_token, token}, state) do
+    #     Task.start(fn ->
+    #         HTTPoison.post!("https://2captcha.com/in.php?key=bbe7d8b5a2b151828c8268ae93334bae&method=userrecaptcha&googlekey=6LfbsiMUAAAAAOu1nGK8InBaFrIk17dcbI0sqvzj&invisible=1&pageurl=https://my.minecraft.net/en-us/redeem/minecraft/#redeem")
+    #     end)
+
+
+    #     {:noreply, %{state | captcha_token: token}}
+    # end
 
     def handle_info({:snipe1}, state) do
         # this is the actual snipe itself
@@ -47,3 +56,13 @@ defmodule OgSniper.Ninja do
         {:noreply, state}
     end
 end
+
+# def attempt_mojang_auth(email, password, captcha) do
+#     HTTPoison.post!("https://authserver.mojang.com/authenticate", Poison.encode!(%{
+#     "captcha" => captcha,
+#     "captchaSupported" => "InvisibleReCAPTCHA",
+#     "password" => password,
+#     "username" => email,
+#     "requestUser" => true
+#      }),[{"Content-Type", "application/json"}])
+# end
--- lib/utils.ex
+++ lib/utils.ex
@@ -35,4 +35,50 @@ defmodule OgSniper.Utils do
                 {:error}
         end
     end
+
+    def send_captcha do
+        %HTTPoison.Response{body: body} = HTTPoison.get!("https://2captcha.com/in.php?key=bbe7d8b5a2b151828c8268ae93334bae&method=userrecaptcha&googlekey=6LfbsiMUAAAAAOu1nGK8InBaFrIk17dcbI0sqvzj&invisible=1&pageurl=https://my.minecraft.net/en-us/redeem/minecraft/#redeem")
+        
+        body
+        |> String.replace_leading(body, "OK|", "")
+        |> IO.puts("captchaToken")
+    end
+
+    def attempt_minecraft_auth(minecraft_email, minecraft_password, captchaToken) do
+        %HTTPoison.Response{body: body} = HTTPoison.post!("https://authserver.mojang.com/authenticate", Poison.encode(%{
+            "captcha" => captchaToken,
+            "captchaSupported" => "InvisibleReCAPTCHA",
+            "password" => password,
+            "username" => email,
+            "requestUser" => true
+            }), [{"Content-Type", "application/json"}])
+        
+        body
+        |> Poison.decode!
+        |> Map.get("accessToken")
+    end
+
+    def snipe_process(accessToken) do
+        HTTPoison.put!(
+            "https://api.mojang.com/user/profile/agent/minecraft/name/name", 
+            nil, 
+            [{"Authorization", "Bearer #{accessToken}"},
+            {"User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"},
+            {"Origin", "https://my.minecraft.net"},
+            {"Referer", "https://my.minecraft.net/en-us/redeem/minecraft/"},
+            {"Accept", "*/*"}]
+        )
+
+        HTTPoison.post!("https://api.mojang.com/token/redeem", Poison.encode(%{
+            "code" => "A1A1A-B2B2B-C3C3C-D4D4D-E5E5E",
+            "languageCode" => "en-us",
+            "productType:" => "GAME"
+            }),
+        [{"Authorization", "Bearer #{accessToken}"},
+        {"Accept", "application/json, text/javascript, */*; q=0.01"},
+        {"Content-Type", "application/json"},
+        {"User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"}]
+        )
+    end
+    
 end
