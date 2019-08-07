0b54b4119ffc475186cf11f9978a01658498b175
Krom <rramin125@gmail.com>
2019-08-07 01:09:21 -0400

    added more snipes

--- lib/og_sniper.ex
+++ lib/og_sniper.ex
@@ -2,12 +2,12 @@ defmodule OgSniper do
   alias OgSniper.Utils
 
   def start do
-    {:ok, time_to_snipe} = Utils.username_to_uuid("osp") 
+    {:ok, time_to_snipe} = Utils.username_to_uuid("deathdead14") 
                            |> Utils.get_change_timestamp
                            |> Utils.add_37_days
                            
     OgSniper.Ninja.start_link(%{
-      desired_name: "osp", 
+      desired_name: "deathdead14", 
       snipe_at_timestamp: time_to_snipe, 
       minecraft_email: "kromislit@gmail.com", 
       minecraft_password: "Krom11!!",
